a2e0b3bd106cdb05405192759401e3128be1196f
tassavarat <tassavarat@gmail.com>
2020-04-19 23:47:57 -0700

    Rewriting logic recursively

--- 0x00-red_black_tree/1-rb_tree_is_valid.c
+++ 0x00-red_black_tree/1-rb_tree_is_valid.c
@@ -1,50 +1,6 @@
 #include "rb_trees.h"
 
-/**
- * pop - removes last node in a linked list
- * @cur_s: double pointer to end of stack node
- * @black_count: current count of black nodes
- *
- * Return: node of cur_s right child, NULL if nothing to pop
- */
-rb_tree_t *pop(stack **cur_s, int *black_count)
-{
-	rb_tree_t *tmp;
-	stack *tmp_s;
-
-	if (!*cur_s)
-		return (NULL);
-	tmp = (*cur_s)->node->right;
-	tmp_s = *cur_s;
-	*black_count = (*cur_s)->black_count;
-	*cur_s = (*cur_s)->prev;
-	free(tmp_s);
-	if (*cur_s)
-		(*cur_s)->next = NULL;
-	return (tmp);
-}
-
-/**
- * push - create and append node to end of linked list
- * @cur_s: double pointer to end of stack node
- * @cur: pointer to current red-black tree node
- * @black_count: current count of black nodes
- *
- * Return: pointer to newly created node, otherwise NULL
- */
-stack *push(stack **cur_s, rb_tree_t *cur, int black_count)
-{
-	stack *new = malloc(sizeof(*new));
-
-	if (!new)
-		return (NULL);
-	new->black_count = black_count;
-	new->node = cur;
-	new->prev = *cur_s;
-	new->next = NULL;
-	*cur_s = new;
-	return (new);
-}
+static size_t MAX_BLACK;
 
 /**
  * key_fail - checks if valid binary search tree
@@ -52,11 +8,14 @@ stack *push(stack **cur_s, rb_tree_t *cur, int black_count)
  *
  * Return: 1 if invalid binary search tree, otherwise 0
  */
-int key_fail(rb_tree_t *cur)
+int key_fail(const rb_tree_t *cur)
 {
 	if ((cur->left && cur->left->n > cur->n) ||
 			(cur->right && cur->right->n < cur->n))
+	{
+		/* puts("key fail"); */
 		return (1);
+	}
 	return (0);
 }
 
@@ -66,17 +25,58 @@ int key_fail(rb_tree_t *cur)
  *
  * Return: 1 if invalid colour properties, otherwise 0
  */
-int colour_fail(rb_tree_t *cur)
+int colour_fail(const rb_tree_t *cur)
 {
-	if ((cur->color != RED && cur->color != BLACK) ||
-			(!cur->parent && cur->color != BLACK))
+	if (cur->color != RED && cur->color != BLACK)
+	{
+		/* puts("invalid colour"); */
 		return (1);
+	}
 	if (cur->color == RED &&
 			((cur->parent && cur->parent->color == RED) ||
 			 (cur->left && cur->left->color == RED) ||
 			 (cur->right && cur->right->color == RED)))
+	{
+		/* puts("adjacent red node"); */
 		return (1);
+	}
 	return (0);
+}
+
+/**
+ * check_rb_tree - recursively checks if red-black tree is valid
+ * @tree: pointer to tree node
+ * @black_count: count of black nodes
+ *
+ * Return: 1 if valid, otherwise 0
+ */
+int check_rb_tree(const rb_tree_t *tree, size_t black_count)
+{
+	if (!tree)
+	{
+		if (!MAX_BLACK)
+			MAX_BLACK = black_count;
+		return (1);
+	}
+	if (colour_fail(tree) || key_fail(tree))
+		return (0);
+	if (tree->color == BLACK)
+		++black_count;
+	/* printf("tree->n %i\tblack_count %lu\tMAX_BLACK %lu\n", */
+	/*		tree->n, black_count, MAX_BLACK); */
+	if (!check_rb_tree(tree->left, black_count) ||
+			!check_rb_tree(tree->right, black_count) ||
+			((!tree->left || !tree->right) &&
+			 black_count != MAX_BLACK))
+		return (0);
+	/* if ((!tree->left || !tree->right) && black_count != MAX_BLACK) */
+	/* { */
+	/*	printf("count difference: tree->n %i\tblack_count" */
+	/*			"%lu\tMAX_BLACK %lu\n", */
+	/*			tree->n, black_count, MAX_BLACK); */
+	/*	return (0); */
+	/* } */
+	return (1);
 }
 
 /**
@@ -87,41 +87,14 @@ int colour_fail(rb_tree_t *cur)
  */
 int rb_tree_is_valid(const rb_tree_t *tree)
 {
-	rb_tree_t *cur;
-	stack *cur_s;
-	int black_count, tmp_count, popped;
+	size_t black_count;
 
-	if (!tree)
-		return (0);
-	cur = (rb_tree_t *) tree;
-	cur_s = NULL;
-	black_count = tmp_count = popped = 0;
-	while (cur)
+	if (!tree || tree->color != BLACK)
 	{
-		if (colour_fail((rb_tree_t *) cur) ||
-				key_fail((rb_tree_t *) cur))
-			return (0);
-		if (cur->color == BLACK)
-			++black_count;
-		/* printf("cur->n\t%i\n", cur->n); */
-		if (cur->right && !push(&cur_s, (rb_tree_t *) cur, black_count))
-			return (0);
-		if (cur->left)
-		{
-			cur = cur->left;
-		}
-		else
-		{
-			/* puts("popped"); */
-			if (!popped)
-				tmp_count = black_count;
-			if (black_count != tmp_count)
-				return (0);
-			/* printf("black_count\t%i\n", black_count); */
-			cur = pop(&cur_s, &black_count);
-			popped = 1;
-		}
-		/* puts("####"); */
+		/* puts("invalid tree / root colour"); */
+		return (0);
 	}
-	return (1);
+	black_count = 0;
+	MAX_BLACK = 0;
+	return (check_rb_tree(tree, black_count));
 }
