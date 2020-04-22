0514d7edbe6bb5d61e4e3fc3f3483464b882b5bb
tassavarat <tassavarat@gmail.com>
2020-04-22 14:55:04 -0700

    Updating documentation

--- 0x00-red_black_tree/3-array_to_rb_tree.c
+++ 0x00-red_black_tree/3-array_to_rb_tree.c
@@ -5,7 +5,8 @@
  * @array: pointer to first element of array to convert
  * @size: number of elements in array
  *
- * Values already present will be ignored
+ * Values already present will be ignored, rb_tree_insert malloc fail will
+ * not return NULL
  * Return: pointer to root node of generated red-black tree, NULL on failure
  */
 rb_tree_t *array_to_rb_tree(int *array, size_t size)
