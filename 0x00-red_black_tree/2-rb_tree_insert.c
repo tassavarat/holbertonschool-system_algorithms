ed85afec99da71af1b9056a45a14b14ec51ba07b
tassavarat <tassavarat@gmail.com>
2020-04-24 19:13:15 -0700

    Returning in rot functions if child is NULL

--- 0x00-red_black_tree/2-rb_tree_insert.c
+++ 0x00-red_black_tree/2-rb_tree_insert.c
@@ -12,6 +12,8 @@ void rotr(rb_tree_t **tree, rb_tree_t *node)
 {
 	rb_tree_t *child = node->left;
 
+	if (!child)
+		return;
 	node->left = child->right;
 	if (child->right)
 		child->right->parent = node;
@@ -38,6 +40,8 @@ void rotl(rb_tree_t **tree, rb_tree_t *node)
 {
 	rb_tree_t *child = node->right;
 
+	if (!child)
+		return;
 	node->right = child->left;
 	if (child->left)
 		child->left->parent = node;
