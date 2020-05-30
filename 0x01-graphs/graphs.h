e41973bb6d4a5228e5f21fe805b8e9b2cab5ef93
tassavarat <tassavarat@gmail.com>
2020-05-29 18:29:13 -0700

    Updating prototype

--- 0x01-graphs/graphs.h
+++ 0x01-graphs/graphs.h
@@ -65,6 +65,7 @@ typedef struct graph_s
 	vertex_t    *vertices;
 } graph_t;
 
+void graph_display(const graph_t *graph);
 graph_t *graph_create(void);
 vertex_t *graph_add_vertex(graph_t *graph, const char *str);
 int graph_add_edge(graph_t *graph, const char *src, const char *dest,
