3c62f4e89ec9508dfc03a86e01114000d9d894a6
tassavarat <tassavarat@gmail.com>
2020-05-31 17:21:31 -0700

    Refactored for one recursive call

--- 0x01-graphs/4-depth_first_traverse.c
+++ 0x01-graphs/4-depth_first_traverse.c
@@ -7,28 +7,22 @@
  * @action: function pointer to be called for each visited vertex
  * @depth: current depth
  * @max_depth: max depth reached during traversal
- *
- * Return: max_depth, 0 on failure
  */
-size_t dfs(vertex_t *v, int *visited,
+void dfs(vertex_t *v, int *visited,
 		void (*action)(const vertex_t *v, size_t depth), size_t depth,
 		size_t *max_depth)
 {
 	edge_t *e;
 
 	if (!v || visited[v->index])
-		return (0);
+		return;
 	visited[v->index] = 1;
 	action(v, depth);
 	if (depth > *max_depth)
 		*max_depth = depth;
-	if (v->edges)
-	{
-		dfs(v->edges->dest, visited, action, ++depth, max_depth);
-		for (e = v->edges->next; e; e = e->next)
-			dfs(e->dest, visited, action, depth, max_depth);
-	}
-	return (*max_depth);
+	++depth;
+	for (e = v->edges; e; e = e->next)
+		dfs(e->dest, visited, action, depth, max_depth);
 }
 
 /**
@@ -52,7 +46,7 @@ size_t depth_first_traverse(const graph_t *graph,
 	if (!visited)
 		return (0);
 	max_depth = 0;
-	max_depth = dfs(graph->vertices, visited, action, 0, &max_depth);
+	dfs(graph->vertices, visited, action, 0, &max_depth);
 	free(visited);
 	return (max_depth);
 }
