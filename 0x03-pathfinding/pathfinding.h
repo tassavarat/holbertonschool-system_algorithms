559d379bef172549a98e5684e4d453a9cd126264
tassavarat <tassavarat@gmail.com>
2020-07-18 22:17:39 -0700

    Updating prototypes

--- 0x03-pathfinding/pathfinding.h
+++ 0x03-pathfinding/pathfinding.h
@@ -40,5 +40,7 @@ queue_t *backtracking_array(char **map, int rows, int cols,
 		point_t const *start, point_t const *target);
 queue_t *backtracking_graph(graph_t *graph, vertex_t const *start,
 		vertex_t const *target);
+queue_t *dijkstra_graph(graph_t *graph, vertex_t const *start,
+		vertex_t const *target);
 
 #endif /* PATHFINDING_H */
