a4294e520b17da222a5dabc701445a4e3d1a0ebb
tassavarat <tassavarat@gmail.com>
2020-08-30 03:02:56 -0700

    Implementing n-ary tree delete

---
+++ 0x04-nary_trees/1-nary_tree_delete.c
@@ -0,0 +1,19 @@
+#include "nary_trees.h"
+
+/**
+ * nary_tree_delete - free entire nary_tree_t tree
+ * @tree: root node of n-ary tree
+ */
+void nary_tree_delete(nary_tree_t *tree)
+{
+	nary_tree_t *tmp;
+
+	while (tree)
+	{
+		tmp = tree;
+		tree = tree->next;
+		nary_tree_delete(tmp->children);
+		free(tmp->content);
+		free(tmp);
+	}
+}
